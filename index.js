3f5dfd597472a2da97f949ae6233448ebc84b9bb
EL HIDAM Loubna <loubna.elhidam@gmail.com>
2020-12-01 12:55:02 +0100

    Node MongoDB Example 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017';
 const dbname = 'conFusion';
@@ -9,22 +10,22 @@ MongoClient.connect(url, (err, client) => {
         console.log('connected correctly to server');
 
         const db = client.db(dbname);
-        const collection = db.collection('dishes');
-        collection.insertOne({'name':'ELHIDAM', 'description':'Test'},(err, result) => {
-            assert.equal(err, null);
-            console.log('After Insert:\n');
-            console.log(result.ops);
-            collection.find({}).toArray((err, docs) => {
-                assert.equal(err, null);
-                console.log('Found:\n');
-                console.log(docs);
+        dboper.insertDocument(db,{name:"vadonut", description:"Test"}, 'dishes', (result) => {
+                 console.log('Insert Document:\n', result.ops);
+                 dboper.findDocuments(db, 'dishes', (docs) => {
+                     console.log('Found Document:\n',docs);
+                     dboper.updateDocument(db, {name: "vadonut"},{description: "Updated Test"},'dishes',(result) => {
+                         console.log('Updated Document:\n',result.result);
+                         dboper.findDocuments(db, 'dishes', (docs) => {
+                            console.log('Found Document:\n',docs);
+                            db.dropCollection('dishes', (result) => {
+                               console.log('droped collection : ', result);
+                               client.close();
+                            });
+                         });
+                     });
+                 });
 
-                db.dropCollection('dishes', (err, result) => {
-                    assert.equal(err, null);
-
-                    client.close();
-                });
-            })
         });
 
 });
